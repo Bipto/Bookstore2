880e07b91fde7e6a8711a6ee5cdd89a0649179aa
Adam Beardow <abeardow13@gmail.com>
2026-09-04 22:55:33 +0100

    Added books to database

--- src/sites/main/php/public/pages/main.php
+++ src/sites/main/php/public/pages/main.php
@@ -1,1 +1,33 @@
-<h1>Hello World</h1>
+<h1>View Books</h1>
+
+<?php
+
+$host = $_ENV['POSTGRES_HOST'];
+$port = $_ENV['POSTGRES_PORT'];
+$dbname = $_ENV['POSTGRES_DB'];
+$user = $_ENV['POSTGRES_USER'];
+$password = $_ENV['POSTGRES_PASSWORD'];
+
+try {
+    $pdo = new PDO(
+        "pgsql:host=$host;port=$port;dbname=$dbname",
+        $user,
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]
+    );
+
+    $stmt = $pdo->prepare("SELECT * FROM bookstore.books");
+    $stmt->execute();
+    $results = $stmt->fetchAll();
+    //var_dump($results);
+
+    foreach ($results as $result) {
+        echo $result['title'] . ' - ' . $result['author'];
+        echo '<br>';
+    }
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
