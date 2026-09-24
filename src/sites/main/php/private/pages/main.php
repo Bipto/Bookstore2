9e1037fd358835089fbcef0d093f2d9a25adff3f
Adam Beardow <abeardow13@gmail.com>
2026-09-24 20:50:44 +0100

    Added spinner to home screen

--- src/sites/main/php/private/pages/main.php
+++ src/sites/main/php/private/pages/main.php
@@ -2,7 +2,7 @@
 
 require_once '/var/www/shared/api.php';
 
-$response = API::get('/books');
+/* $response = API::get('/books');
 $results = json_decode($response, true);
 
 if ($results['success']) {
@@ -35,3 +35,20 @@ if ($results['success']) {
 } else {
     echo '<h1>Failed to connect to database and retrieve books<h1>';
 }
+ */
+
+/* echo '
+<div class="container">
+<div id="overlay">
+<div class="spinner"></div>
+</div>
+</div>'; */
+
+echo '<div class="page">
+<div class="container">
+
+<div class="spinner">
+</div>
+
+</div>
+</div>';
